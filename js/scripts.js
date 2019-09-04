556786ae186f39acc099df0c97179babea1f1227
Brian Odida <bryodiiidah@gmail.com>
2019-09-04 21:56:37 +0300

    This loops through the number of teachers and stores their names in an array

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@ for (var i = 1; i <= numberOfTeachers[0]; i++) {
     $("#one").append('<form class="teacherDetails">' + '<label for="teachersName' + i + '" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName' + i + '>' + '<br><br>' + '<label for="teachersId' + i + '">Teacher\'s Id:</label>' + '<input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId' + i + '">' + '</form><hr>')
 
 }
+
+for (var j = 1; j <= numberOfTeachers[0]; j++) {
+    teacherNames.push($("#teachersName" + j).val());
+}
