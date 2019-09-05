abbae89dbe782d3dc58808c69e48cb91390d6721
Brian Odida <bryodiiidah@gmail.com>
2019-09-05 09:37:57 +0300

    This adds instructions for the user to enter a certain input fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,35 @@
 //UI Logic
 
 $(document).ready(function() {
-    numberOfTeachers.push(8);
 
-    for (var i = 1; i <= numberOfTeachers[0]; i++) {
-        $("#one").append('<form class="teacherDetails">' + '<label for="teachersName' + i + '" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName' + i + '>' + '<br><br>' + '<label for="teachersId' + i + '">Teacher\'s Id:</label>' + '<input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId' + i + '">' + '</form><hr>')
+    $("#next").click(function() {
+        numberOfTeachers.push($("#teachers2").val())
 
-    }
+        for (var i = 1; i <= numberOfTeachers[0]; i++) {
+            $("#teacherInput").append('<form class="teacherDetails">' + '<label for="teachersName' + i + '" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName' + i + '>' + '<br><br>' + '<label for="teachersId' + i + '">Teacher\'s Id:</label>' + '<input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId' + i + '">' + '</form><hr>')
+
+        }
+
+        numberOfSubjects.push($("#subjects2").val());
+
+        for (var k = 1; k <= numberOfSubjects[0]; k++) {
+            $("#numberOfSubjects").append('<form><label for=subjectName' + k + '>Name of Subject:</label><input type="text" class="form-group" placeholder="Name OF Subject" id="subjectName"' + k + '></form>')
+        }
+
+        $("#next").hide();
+        $(".hidden").hide();
+        $("#teachers").append("<h3>Please Input the names and ids of all the " + numberOfTeachers[0] + " teachers in the respective fields</h3>")
+    });
+
+
 
     for (var j = 1; j <= numberOfTeachers[0]; j++) {
         teacherNames.push($("#teachersName" + j).val());
     }
 
-    numberOfSubjects.push(5);
 
-    for (var k = 1; k <= numberOfSubjects[0]; k++) {
-        $("#two").append('<form><label for=subjectName' + k + '>Name of Subject:</label><input type="text" class="form-group" placeholder="Name OF Subject" id="subjectName"' + k + '></form>')
-    }
+
+
 
     for (var m = 1; m <= numberOfSubjects[0]; m++) {
         nameOfSubjects.push($("#subjectName" + m).val());
