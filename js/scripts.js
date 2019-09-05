9d4e04c75dbf6f7a1ecc034b100fea5b8017939c
Brian Odida <bryodiiidah@gmail.com>
2019-09-05 12:01:38 +0300

    This collects the user input in an array and appends randomly on a table

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
         numberOfTeachers.push($("#teachers2").val())
 
         for (var i = 1; i <= numberOfTeachers[0]; i++) {
-            $("#teacherInput").append('<form class="teacherDetails">' + '<label for="teachersName' + i + '" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName' + i + '>' + '<br><br>' + '<label for="teachersId' + i + '">Teacher\'s Id:</label>' + '<input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId' + i + '">' + '</form><hr>')
+            $("#teacherInput").append('<div class=" form-group teacherDetails">' + '<label for="teachersName' + i + '" >Teacher\'s Name:</label><input type="text" class="form-control" placeholder="Teacher\'s Name" id="teachersName' + i + '>' + '<br><br>' + '<label for="teachersId' + i + '">Teacher\'s Id:</label>' + '<input type="text" class="form-control" placeholder="Teacher\'s Id" id="teachersId' + i + '">' + '</div><hr>')
 
         }
 
         numberOfSubjects.push($("#subjects2").val());
 
         for (var k = 1; k <= numberOfSubjects[0]; k++) {
-            $("#numberOfSubjects").append('<form><label for=subjectName' + k + '>Name of Subject:</label><input type="text" class="form-control" placeholder="Name OF Subject" id="subjectName"' + k + '></form>')
+            $("#numberOfSubjects").append('<div class="form-group" ><label for=subjectName' + k + '>Name of Subject:</label><input type="text" class="form-control" placeholder="Name OF Subject" id="subjectName' + k + '"</div>')
         }
         $("#submitForm").show();
         $("#next").hide();
@@ -23,26 +23,31 @@ $(document).ready(function() {
     });
 
     $("#submit").click(function() {
+
+        event.preventDefault();
+
+        for (var j = 1; j <= numberOfTeachers[0]; j++) {
+            teacherNames.push($("#teachersName" + j).val());
+        }
+
+
+        for (var m = 1; m <= numberOfSubjects[0]; m++) {
+            subjectName.push($("input#subjectName" + m).val());
+        }
+
+
+        $("#subjectName7").css("color", "yellow");
+
+        $(".td").each(function() {
+            $(this).text(ranSubject());
+        });
+
         $(".table-responsive").show();
         $("#forms").hide();
         $("#submitForm").hide();
-    })
 
 
-
-    for (var j = 1; j <= numberOfTeachers[0]; j++) {
-        teacherNames.push($("#teachersName" + j).val());
-    }
-
-
-
-
-
-    for (var m = 1; m <= numberOfSubjects[0]; m++) {
-        nameOfSubjects.push($("#subjectName" + m).val());
-    }
-
-    $(".td").each(function() {
-        $(this).text(ranSubject());
     });
+
+
 });
